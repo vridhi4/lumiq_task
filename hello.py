ea0514a4bdae0701be35e9b8359d174af52fc7ef
vridhi <vridhikatariya32@gmail.com>
2024-02-26 17:53:54 +0530

    sum of two numbers in python

---
+++ hello.py
@@ -0,0 +1,14 @@
+def main():
+    # Get input from the user
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    # Calculate the sum
+    total = num1 + num2
+
+    # Print the sum
+    print("The sum of", num1, "and", num2, "is:", total)
+
+if __name__ == "__main__":
+    main()
+

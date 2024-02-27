886be4ea2e0455b23101f628ed78f2ad6cb696d8
vridhi <vridhikatariya32@gmail.com>
2024-02-27 11:59:36 +0530

    sum of three numbers

--- hello.py
+++ hello.py
@@ -2,12 +2,12 @@ def main():
     # Get input from the user
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
-
+    num3 = float(input("Enter the third number: "))
     # Calculate the sum
-    total = num1 + num2
+    total = num1 + num2 + num3
 
     # Print the sum
-    print("The sum of", num1, "and", num2, "is:", total)
+    print("The sum of", num1, num2, "and", num3, "is:", total)
 
 if __name__ == "__main__":
     main()
